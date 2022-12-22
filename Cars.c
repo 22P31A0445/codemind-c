f273032a08f2682d0e76fa4a37eeaf4ed211936f
22P31A0445 <119718890+22P31A0445@users.noreply.github.com>
2022-12-22 21:39:55 +0530

    #include<stdio.h> int main() {     int n,m;     scanf("%d",&n);     m=n/4;     if(n%4==0)     {         printf("%d",m);     }     else     {         printf("%d",m+1);     } }

---
+++ Cars.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main()
+{
+    int n,m;
+    scanf("%d",&n);
+    m=n/4;
+    if(n%4==0)
+    {
+        printf("%d",m);
+    }
+    else
+    {
+        printf("%d",m+1);
+    }
+}
