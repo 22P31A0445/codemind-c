283017e82833f628a15a3542b1e68121904aac0b
22P31A0445 <119718890+22P31A0445@users.noreply.github.com>
2023-01-20 10:22:46 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             printf("%c ",64+i);         }         printf(" ");     }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
     int i,j,n;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=1;j<=i;j++)
+        for(j=1;j<=n;j++)
         {
-            printf("%d",j);
+            printf("%c ",64+i);
         }
         printf("
 ");
     }
     
 }
+
